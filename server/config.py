efd69f4e80d4a75438b6dc06c1f5e3f3ea496476
Trevor <Trevor.T.Glasgow.20@dartmouth.edu>
2020-04-13 13:26:50 -0500

    Restart

--- server/config.py
+++ server/config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'quantum-is-cool'
+    SECRET_KEY = 'a-man-a-plan-a-canal-panama'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
